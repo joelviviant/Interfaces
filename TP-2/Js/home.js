a827df1c94536a05fdc3df00c626bd21611cc83e
joelviviant <joelviviant31@gmail.com>
2023-10-05 00:42:52 -0300

    PRE LOADER

--- TP-2/Js/home.js
+++ TP-2/Js/home.js
@@ -26,3 +26,12 @@ function slideDer(btnDer) {
     let carrousel = aux.firstElementChild;
     carrousel.style.transform = `translateX(${slide}px)`;
 }
+document.body.classList.add('no-scroll');
+
+// Esperar 5 segundos y ocultar el preloader
+setTimeout(function() {
+  document.querySelector('.preloader').classList.add('hide');
+  
+  // Habilitar el scroll nuevamente
+  document.body.classList.remove('no-scroll');
+}, 5000);
