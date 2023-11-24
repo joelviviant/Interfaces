4af485fc1365f11a8f5662632b51004d45402ecd
joel <joelviviant31@gmail.com>
2023-11-24 14:44:26 -0300

    pre loader

--- TP-4/js/main.js
+++ TP-4/js/main.js
@@ -152,3 +152,24 @@ document.addEventListener("scroll", () => {
 
     console.log(posScroll);
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+       
+        document.querySelector(".preloader").style.display = "none";
+    }, 4000); 
+
+    let progressBar = document.querySelector(".loader-bar");
+    let progressText = document.querySelector(".loader-text");
+
+    let interval = setInterval(function () {
+        if (progressBar.style.width === "100%") {
+            clearInterval(interval);
+        } else {
+            let currentWidth = parseFloat(progressBar.style.width) || 0;
+            let newWidth = Math.min(currentWidth + 10, 100);
+            progressBar.style.width = newWidth + "%";
+            progressText.textContent = "Cargando... " + Math.round(newWidth) + "%";
+        }
+    }, 200);
+});
